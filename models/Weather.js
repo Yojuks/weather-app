a421f70e432917a26ac9f1c2fe921d1f6c5f460b
AndriiYozh <yojuks@gmail.com>
2022-02-21 23:06:03 +0200

    Add model and routes

---
+++ models/Weather.js
@@ -0,0 +1,11 @@
+const { Schema, model, Types } = require("mongoose");
+
+var schema = new Schema({
+  // owner: { type: Types.ObjectId, ref: "User" },
+  city: { type: String },
+  temperature: { type: String },
+  description: { type: String },
+  icon: { type: String },
+});
+
+module.exports = model("Weather", schema);
