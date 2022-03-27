eb7c84981665c7187f799df88b9c84a8f38c4d12
AndriiYozh <yojuks@gmail.com>
2022-03-27 14:21:32 +0300

    Change Mongo model

--- models/Weather.js
+++ models/Weather.js
@@ -1,7 +1,6 @@
 const { Schema, model, Types } = require("mongoose");
 
 var schema = new Schema({
-  // owner: { type: Types.ObjectId, ref: "User" },
   city: { type: String },
   temperature: { type: String },
   description: { type: String },
